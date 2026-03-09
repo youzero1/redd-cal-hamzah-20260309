30f7bb9375ce2d0c17ee85ea745e39e688103b6a
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-09 16:48:06 +0500

    Add redd-cal/src/entities/CalculationHistory.ts

---
+++ redd-cal/src/entities/CalculationHistory.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('calculation_history')
+export class CalculationHistory {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text' })
+  expression!: string;
+
+  @Column({ type: 'text' })
+  result!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
